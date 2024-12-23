9927b288a648e4658b11b518b1f64f4f38a39b08
xxkklose <2440842033@qq.com>
2024-12-23 16:19:50 +0800

    update color

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -194,4 +194,14 @@ return {
       },
     },
   },
+
+  -- use markdown-preview.nvim
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 }
